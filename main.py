99206ff53efc68c442ee7d2dc0ffe9ffb2bd1e5d
Aleksandr <alexandr44@inbox.lv>
2023-03-08 18:06:54 +0200

    Update apps

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ def form_authorization():
         return render_template ("auth_bad.html")
       cursor_db.execute(('''SELECT parole FROM passwords WHERE login = '{}';''').format(login))
       pwd = cursor_db.fetchone()
-      print(pwd)
       cursor_db.close()
       pwd=pwd[0]  
       p = check_password_hash(pwd,parole)
@@ -52,10 +51,8 @@ def form_registration():
    if request.method == 'POST':
        login = request.form.get('Login')
        parole = request.form.get('Password')
-       # psw = generate_password_hash(parole)
        db_lp = sqlite3.connect('login_password.db')
        cursor_db = db_lp.cursor()
-       # sql_insert = '''INSERT INTO passwords VALUES('{}','{}');'''.format(Login, psw)
        sql_insert = '''INSERT INTO passwords VALUES('{}','{}');'''.format(login,generate_password_hash(parole))
        cursor_db.execute(sql_insert)
        cursor_db.close()
