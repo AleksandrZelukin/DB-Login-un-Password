6259e54f2b081cf4ba09491a1a9a343e0aef209a
Aleksandr <alexandr44@inbox.lv>
2023-03-11 18:01:23 +0200

    Update apps

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ id_user INTEGER PRIMARY KEY AUTOINCREMENT,
 login TEXT,
 parole TEXT);''')
 db.commit()
-
 sql.close()
 db.close()
 
@@ -76,27 +75,3 @@ def pass_generator():
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port='8080',debug=True)
 
-
-# @app.route('/authorization', methods=['GET', 'POST'])
-# def form_authorization():
-#     if request.method == 'POST':
-#       login = request.form.get('Login')
-#       parole = request.form.get('Password')
-#       db = sqlite3.connect('login_password.db')
-#       sql = db.cursor()
-#       info = sql.execute(('''SELECT login FROM passwords WHERE login = '{}';''').format(login)).fetchone()
-#       if info is None:
-#         return render_template ("auth_bad.html")
-#       sql.execute(('''SELECT parole FROM passwords WHERE login = '{}';''').format(login))
-#       pwd = sql.fetchone()
-#       print(pwd)
-#       sql.close()
-#       pwd=pwd[0]  
-#       p = check_password_hash(pwd,parole)
-#       try:
-#           if not p:
-#                return render_template('auth_bad.html')
-#       except:
-#            return render_template('auth_bad.html')
-#       return render_template('successfulauth.html')
-#     return render_template('authorization.html')
